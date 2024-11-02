769546b9f39a8ee377c5172e749e532cc304392b
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2024-11-02 10:48:21 +0530

    first commit

---
+++ day4.js
@@ -0,0 +1,46 @@
+// 1.var a=["sai","sweety"]
+// console.log(a.length)
+// o/p:2
+
+
+// 2. var arr=["sai","sweety",]
+// arr.push("sonu");
+// console.log(arr);
+//  o/p:(3) ['sai', 'sweety', 'sonu']
+
+// 3. var arr=["01","02","03"]
+// arr.pop( );
+// console.log(arr);
+// o/p:(2) ['01', '02']
+
+// 4.var arr=["sai","sweety",]
+// arr.unshift("sonu");
+// console.log(arr);
+// output:(3) ['sonu', 'sai', 'sweety']
+
+// 5.var arr=['sonu', 'sai', 'sweety']
+// arr.shift( )
+// console.log(arr)
+// o/p:(2) ['sai', 'sweety']
+
+// 6.var arr=["sai","sweety",]
+// console.log(arr.reverse());
+// o/p:(2) ['sweety', 'sai']
+
+// 7.var arr=["sai","sweety","srinu","kumari"]
+// console.log(arr.indexOf("srinu"));
+// console.log(arr.indexOf("kumari"));
+// o/p:2
+//     3
+
+// 10.var arr="srinu"
+// console.log(arr.split(""));
+// o/p:(5) ['s', 'r', 'i', 'n', 'u']
+
+// 9. var arr=["10","50","70","24","27"]
+// console.log(arr.sort());
+// o/p:(5) ['10', '24', '27', '50', '70']
+
+// 8. var arr=["10","50","70","24","27"]
+// console.log(arr.indexOf() );
+// o/p: -1
